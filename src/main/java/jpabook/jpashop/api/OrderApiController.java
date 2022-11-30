2970233209f74b09af8d0896c93edc7982996fa6
sojeonglee <sojeonglee0125@gmail.com>
2022-11-30 22:14:24 +0900

    feat : 주문 컬렉션 조회 및 DTO변환 V3 -> fetch 조인 최적화

--- src/main/java/jpabook/jpashop/api/OrderApiController.java
+++ src/main/java/jpabook/jpashop/api/OrderApiController.java
@@ -41,6 +41,14 @@ public class OrderApiController {
                 .collect(Collectors.toList());
     }
 
+    @GetMapping("/api/v3/orders")
+    public List<OrderDto> ordersV3() {
+        List<Order> orders = orderRepository.findAllWithItem();
+        return orders.stream()
+                .map(o -> new OrderDto(o))
+                .collect(Collectors.toList());
+    }
+
     @Data
     static class OrderDto {
         private Long orderId;
